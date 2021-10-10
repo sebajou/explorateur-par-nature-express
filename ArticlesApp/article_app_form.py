b15d062413dee375623cccc10b93ecfd5e865cfe
sebajou <sebajou@gmail.com>
2021-10-10 15:24:23 +0200

    Improve front for author

--- ArticlesApp/article_app_form.py
+++ ArticlesApp/article_app_form.py
@@ -1,23 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
 from ArticlesApp.models import Article, Image, Equipment
-from UsersApp.models import Badge
 from django import forms
 
 
 class ArticleCreationForm(forms.ModelForm):
     """Form for fill article creation. """
-
-    # Add video, image and badge from many to many and many to one relationship
-    # badge_name = forms.MultipleChoiceField(
-    #     required=True,
-    #     widget=forms.CheckboxSelectMultiple,
-    #     choices=Badge.objects.all(),
-    # )
-
-    # image_load = forms.ModelChoiceField(
-    #     required=False,
-    #     queryset=Image.objects.all()
-    # )
 
     class Meta:
         model = Article
@@ -26,7 +12,6 @@ class ArticleCreationForm(forms.ModelForm):
 
 
 class ImageUploadForm(forms.ModelForm):
-    # image = forms.ImageField(label='image_article')
 
     class Meta:
         model = Image
@@ -39,11 +24,3 @@ class EquipmentForm(forms.ModelForm):
         model = Equipment
         fields = ('text', 'quantity', 'unity')
 
-# class BadgeForm(forms.ModelForm):
-#     """Form for fill article creation. """
-#
-#     # Add video, image and badge from many to many and many to one relationship
-#
-#     class Meta:
-#         model = Badge
-#         fields = ('title', 'category', 'level', 'image_badge')
